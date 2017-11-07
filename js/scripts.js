b4047c032174d9c90d76f30a2b28abdf8ee5c67d
Hussein mulla <mullaboxer@gmail.com>
2017-11-07 10:06:45 +0300

    add index.html

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+// front end logic
+
+functionrollDice() {
+  var die1 = document.getElementById('die1');
+  var die2 = document.getElementById('die2');
+  var status = document.getElementById('status');
+  var d1 = Math.floor(Math.random()*6) + 1;
+  var d2 = Math.floor(Math.random()*6) + 1;
+
+
+}
+
+
+
+
+// back end logic
+
+document.getElementById('dice').innerHtml=rollDice+"  and  "+rollDice2
