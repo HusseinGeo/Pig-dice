621d26d1ffde93f2454837cef96da0def425d135
Hussein mulla <mullaboxer@gmail.com>
2017-11-08 23:06:09 +0300

    made changesto index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
+//
+//
+// function roll() {
+//   var dic1 = document.getElementById('dice1');
+//
+//   var d1 = Math.floor(Math.random() * 6 +1);
+//
+//   dice1.innerHtml = d1;
+//
+//   var score = d1;
+//
+// }
+function roll() {
+  var dic1 = document.getElementById('dice1');
 
+  var d1 =Math.floor(Math.random() * 6 +1);
 
-functionrollDice(); {
-  var die1 = document.getElementById('die1');
   var status = document.getElementById('status');
-  var d1 = Math.floor(Math.random()*6) + 1;
 
+  var score = d1;
 
-die1.innerHtml = die1
-status.innerHtml = 'You rolled' + d1 + '.'
+  dice1.innerHTML = d1;
 
-if (die1 === 1) {
-  status.innerHtml = "Sorry! Next player's turn!"
+  status.innerHTML = " You rolled  "   +score+  ".";
 
+  if (d1 == 1) {
+    status.innerHTML += "  Sorry!  your turn is over!";
   }
-};
+}
