cb10b1a5e23fce1e65c7e8b8bd961bd3ccb86e08
Hussein mulla <mullaboxer@gmail.com>
2017-11-08 16:21:10 +0300

    added changes to index.html,css

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
-// front end logic
 
-functionrollDice() {
+
+functionrollDice(); {
   var die1 = document.getElementById('die1');
-  var die2 = document.getElementById('die2');
   var status = document.getElementById('status');
   var d1 = Math.floor(Math.random()*6) + 1;
-  var d2 = Math.floor(Math.random()*6) + 1;
 
 
-}
+die1.innerHtml = die1
+status.innerHtml = 'You rolled' + d1 + '.'
 
+if (die1 === 1) {
+  status.innerHtml = "Sorry! Next player's turn!"
 
-
-
-// back end logic
-
-document.getElementById('dice').innerHtml=rollDice+"  and  "+rollDice2
+  }
+};
